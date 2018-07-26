a577b9a5824627f29b3a308cdb1b06bc14007d05
frique92 <29735207+frique92@users.noreply.github.com>
2018-07-26 22:56:12 +0300

    added server by websocket

--- js/routes.js
+++ js/routes.js
@@ -1,79 +1,102 @@
 ymaps.ready(init);
 
-function init() 
-{
+function init() {
     var maxPoints = 30,
-        delayInterval = 1000;
+        delayInterval = 1000,
+        scaling = true,
         maxPointsInput = document.params.max_points,
         delayInput = document.params.delay,
-		applyButton = document.params.apply,
-        yMap = new ymaps.Map("map", {
-		    center      : [56.13, 40.4],
-            zoom        : 10,
-            controls    : []
-        });
-        
+        applyButton = document.params.apply,
+        scalingInput = document.params.scaling,
+        yMap = new ymaps.Map("map",
+            {
+                center: [56.13, 40.4],
+                zoom: 10,
+                controls: []
+            }),
+        socket = new WebSocket("ws://localhost:8081"),
+        routePolyline = new ymaps.Polyline([], null, 
+            {
+                balloonCloseButton: false,
+                strokeColor: "#0687b6",
+                strokeWidth: 5,
+                strokeOpacity: 0.7
+            }),
+        planePlacemark = new ymaps.Placemark([], null, 
+            {
+                iconLayout: "default#image",
+                iconImageHref: "img/plane.png",
+                iconImageSize: [40, 40],
+                iconImageOffset: [-20, -20]
+            });
+
     maxPointsInput.value = maxPoints;
     delayInput.value = delayInterval / 1000;
+    scalingInput.checked = scaling;
 
-    applyButton.onclick = function(e){
+    applyButton.onclick = function (e) 
+    {
         e.preventDefault();
         maxPoints = maxPointsInput.value;
         delayInterval = delayInput.value * 1000;
-        console.log(delayInterval)
-    }    
+        scaling = scalingInput.checked;
+        socket.send(delayInterval);
+    }
 
-    function getNewCoordinatesPlane(routePolyline)
+    var imageStateServer = document.createElement('img');
+    imageStateServer.src = "img/server_off.png";
+    imageStateServer.id = "img-state-server";
+    document.getElementById('server').appendChild(imageStateServer);
+
+    socket.onmessage = function (event) {
+        let incomingMessage = JSON.parse(event.data);
+        setNewGeometry(routePolyline, planePlacemark, maxPoints, incomingMessage);
+    };
+
+    socket.onopen = function () {
+        imageStateServer.src = "img/server_on.png";
+    };
+
+    socket.onclose = function (event) {
+        imageStateServer.src = "img/server_off.png";
+    };
+
+    function getNewCoordinatesPlane(routePolyline) 
     {
         let lengthGeometry = routePolyline.geometry.getLength(),
             lastCoordinates = routePolyline.geometry.get(lengthGeometry - 1);
 
         return lengthGeometry == 0
-        ? [56.13, 40.4]
-        : lastCoordinates.map(coord => coord - Math.random() / 100 );
+            ? [56.13, 40.4]
+            : lastCoordinates.map(coord => coord - Math.random() / 100);
     }
 
-    function setNewGeometry(routePolyline, planePlacemark, maxPoints)
-	{
-		let lengthGeometry = routePolyline.geometry.getLength(),
-			NewCoordinatesPlane = getNewCoordinatesPlane(routePolyline),
-			boundsPolyline;
+    function setNewGeometry(routePolyline, planePlacemark, maxPoints, NewCoordinatesPlane) 
+    {
+        let lengthGeometry = routePolyline.geometry.getLength(),
+            boundsPolyline;
 
-	    routePolyline.geometry.set(lengthGeometry, NewCoordinatesPlane);
-	    lengthGeometry = routePolyline.geometry.getLength();
+        if (lengthGeometry == 0) 
+        {
+            yMap.geoObjects.add(routePolyline);
+            yMap.geoObjects.add(planePlacemark);
+        }
 
-	    if (lengthGeometry - maxPoints > 0)
-	    	routePolyline.geometry.splice(0, lengthGeometry - maxPoints - 1);
+        routePolyline.geometry.set(lengthGeometry, NewCoordinatesPlane);
+        lengthGeometry = routePolyline.geometry.getLength();
 
-	    boundsPolyline = routePolyline.geometry.getBounds();
+        if (lengthGeometry - maxPoints > 0)
+            routePolyline.geometry.splice(0, lengthGeometry - maxPoints - 1);
 
-	    yMap.setBounds(boundsPolyline, {
-	    	checkZoomRange : true
-        });
-        
+        boundsPolyline = routePolyline.geometry.getBounds();
+
+        if (scaling == true)
+            yMap.setBounds(boundsPolyline, 
+                {
+                    checkZoomRange: true
+                });
+
         planePlacemark.geometry.setCoordinates(NewCoordinatesPlane);
-	}
+    }
 
-	let routePolyline = new ymaps.Polyline([], null, {
-		balloonCloseButton : false,
-		strokeColor        : "#0687b6",
-		strokeWidth        : 5,
-		strokeOpacity      : 0.7
-  	});
-
-    let planePlacemark = new ymaps.Placemark([], null, {
-	    iconLayout      : "default#image",
-	    iconImageHref   : "img/plane.png",
-	    iconImageSize   : [40, 40],
-	    iconImageOffset : [-20, -20]
-	});
-
-    yMap.geoObjects.add(routePolyline);
-    yMap.geoObjects.add(planePlacemark);
-
-    var timerId = setTimeout(function tick() {
-        setNewGeometry(routePolyline, planePlacemark, maxPoints);
-        timerId = setTimeout(tick, delayInterval);
-      }, delayInterval);
-        
 }
---
+++ server/server.js
@@ -0,0 +1,31 @@
+var http = require('http'),
+    Static = require('node-static'),
+    WebSocketServer = new require('ws'),
+    lastCoordinates = [56.13, 40.4],
+    delayInterval = 1000,
+    webSocketServer = new WebSocketServer.Server({ port: 8081 });
+
+webSocketServer.on('connection', function (ws) {
+    setTimeout(function tick() 
+        {
+            let message = getNewCoordinatesPlane(lastCoordinates);
+
+            ws.send(JSON.stringify(message));
+            setTimeout(tick, delayInterval);
+        }, delayInterval);
+
+    ws.on('message', function (message) 
+        {
+            delayInterval = Number(message);
+        });
+
+});
+
+function getNewCoordinatesPlane() 
+{
+    lastCoordinates = lastCoordinates.map(coord => coord - Math.random() / 100);
+    return lastCoordinates;
+}
+
+console.log("Сервер запущен на порте 8081");
+
